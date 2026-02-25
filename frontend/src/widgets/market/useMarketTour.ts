3b699d055b070594ad83f0460317a577a6d4f903
yumao233 <unitwk@outlook.com>
2026-02-25 16:33:51 +0800

    feat: add useMarketTour()

--- frontend/src/widgets/market/useMarketTour.ts
+++ frontend/src/widgets/market/useMarketTour.ts
@@ -26,20 +26,20 @@ export function useMarketTour(isAdmin: MaybeRef<boolean>) {
     if (unref(isAdmin)) {
       steps.push({
         target: () => step1Ref.value ?? null,
-        title: $t("导入整合包/压缩包/开服包"),
-        description: $t("如果需要导入服务端游戏整合包，或者开服包，可以通过这里的方式。")
+        title: $t("TXT_CODE_1bfd647d"),
+        description: $t("TXT_CODE_b5be5a6a")
       });
       steps.push({
         target: () => step2Ref.value ?? null,
-        title: $t("使用 Docker 镜像开服"),
-        description: $t("对于进阶用户，在 Linux 系统下，可以通过此处来基于镜像创建，支持任何镜像。")
+        title: $t("TXT_CODE_963e00f"),
+        description: $t("TXT_CODE_572ba7f0")
       });
     }
     steps.push({
       target: () => step3Ref.value ?? null,
-      title: $t("使用预设包快速创建"),
+      title: $t("TXT_CODE_d284d8a9"),
       description: $t(
-        "如果是第一次创建游戏服务器，可以来这里选择我们的预设包哦～，选择后也支持后续再重新安装更改的！"
+        "TXT_CODE_22814776"
       )
     });
     return steps as any;
